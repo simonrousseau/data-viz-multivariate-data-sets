92468ce6590c177fdcb66360c9be5b23f9351f94
Simon <simonrousseau22@gmail.com>
2019-05-27 18:42:35 +0200

    Add ChartMonth Component

---
+++ src/components/ChartMonth.vue
@@ -0,0 +1,16 @@
+<script>
+import { Line } from 'vue-chartjs'
+import zoom from 'chartjs-plugin-zoom';
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted () {
+    this.addPlugin(zoom);
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style>
+</style>
